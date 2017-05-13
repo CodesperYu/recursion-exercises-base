e9de83afaa038d8ace5526b6f9bb9219b0893ce6
Jasper Yu <jasperyu.kk@gmail.com>
2017-05-12 20:05:02 -0700

    finish testing

--- dominator.js
+++ dominator.js
@@ -2,15 +2,30 @@ const { flattenTreeToArray } = require('./dom-util');
 const _ = require('underscore');
 
 const getElementById = function(root, id) {
-  // Your code here
+  const treeArray = flattenTreeToArray(root);
+  for(let i = 0; i < treeArray.length; i++) {
+    if(treeArray[i].id === id) {
+      return treeArray[i];
+    }
+  }
 };
 
 const getElementsByClassName = function(root, className) {
-  // Your code here
+  const treeArray = flattenTreeToArray(root);
+  const results = [];
+  for(let i = 0; i < treeArray.length; i++) {
+    if(treeArray[i].className && (treeArray[i].className).indexOf(className) !== -1) {
+      results.push(treeArray[i]);
+    }
+  }
+  return results;
 };
 
 const getElementsByTagName = function(root, tagName) {
-  // Your code here
+  const treeArray = flattenTreeToArray(root);
+  return _.filter(treeArray, function(element) {
+    return element.tagName === tagName;
+  });
 };
 
 module.exports = {
@@ -18,3 +33,31 @@ module.exports = {
   getElementsByClassName: getElementsByClassName,
   getElementsByTagName: getElementsByTagName
 };
+
+
+// const getElementById = function(root, id) {
+//   const treeArray = flattenTreeToArray(root);
+//   for(let i = 0; i < treeArray.length; i++) {
+//     if(treeArray[i].id === id) {
+//       return treeArray[i];
+//     }
+//   }
+// };
+
+// const getElementsByClassName = function(root, className) {
+//   const treeArray = flattenTreeToArray(root);
+//   const results = [];
+//   for(let i = 0; i < treeArray.length; i++) {
+//     if(treeArray[i].className && (treeArray[i].className).indexOf(className) !== -1) {
+//       results.push(treeArray[i]);
+//     }
+//   }
+//   return results;
+// };
+
+// const getElementsByTagName = function(root, tagName) {
+//   const treeArray = flattenTreeToArray(root);
+//   return _.filter(treeArray, function(element) {
+//     return element.tagName === tagName;
+//   });
+// };
