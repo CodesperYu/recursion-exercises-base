1c2ddbdf4234ce02de14d5a1980839daa7546732
CodesperYu <jasperyu.kk@gmail.com>
2017-05-12 20:31:31 -0700

    Update dominator.js

--- dominator.js
+++ dominator.js
@@ -33,31 +33,3 @@ module.exports = {
   getElementsByClassName: getElementsByClassName,
   getElementsByTagName: getElementsByTagName
 };
-
-
-// const getElementById = function(root, id) {
-//   const treeArray = flattenTreeToArray(root);
-//   for(let i = 0; i < treeArray.length; i++) {
-//     if(treeArray[i].id === id) {
-//       return treeArray[i];
-//     }
-//   }
-// };
-
-// const getElementsByClassName = function(root, className) {
-//   const treeArray = flattenTreeToArray(root);
-//   const results = [];
-//   for(let i = 0; i < treeArray.length; i++) {
-//     if(treeArray[i].className && (treeArray[i].className).indexOf(className) !== -1) {
-//       results.push(treeArray[i]);
-//     }
-//   }
-//   return results;
-// };
-
-// const getElementsByTagName = function(root, tagName) {
-//   const treeArray = flattenTreeToArray(root);
-//   return _.filter(treeArray, function(element) {
-//     return element.tagName === tagName;
-//   });
-// };
